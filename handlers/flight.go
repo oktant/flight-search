44cd988d45506ac40ecc5d39401db14833775807
oalizada <oalizada@volvocars.com>
2024-02-07 12:23:00 +0100

    bump: release

--- handlers/flight.go
+++ handlers/flight.go
@@ -6,22 +6,22 @@ import (
 	"github.com/srinivasvinay/flight-search/models"
 	"net/http"
 )
+
 var da dao.GormDatabase
 
-func GetFlights(c echo.Context) (err error){
+func GetFlights(c echo.Context) (err error) {
 	source := c.QueryParam("source")
 	destination := c.QueryParam("destination")
-	if len(source) >0 && len(destination) >0 {
+	if len(source) > 0 && len(destination) > 0 {
 		return c.JSON(http.StatusOK, da.GetFlightsBySourceAndDestination(source, destination))
-
 	}
 	return c.JSON(http.StatusOK, da.GetAllFlights())
 }
 
-func CreateFlight(c echo.Context) (err error){
-	flight := new (models.Flight)
-	if err=c.Bind(flight); err !=nil {
-			return echo.NewHTTPError(http.StatusOK, "can't bind")
+func CreateFlight(c echo.Context) (err error) {
+	flight := new(models.Flight)
+	if err = c.Bind(flight); err != nil {
+		return echo.NewHTTPError(http.StatusOK, "can't bind")
 	}
 
 	return c.JSON(http.StatusOK, da.CreateNewFlight(flight))
@@ -33,8 +33,8 @@ func GetBookings(c echo.Context) (err error) {
 }
 
 func CreateBooking(c echo.Context) (err error) {
-	booking := new (models.Booking)
-	if err=c.Bind(booking); err !=nil {
+	booking := new(models.Booking)
+	if err = c.Bind(booking); err != nil {
 		return echo.NewHTTPError(http.StatusOK, "can't bind")
 	}
 	return c.JSON(http.StatusOK, da.CreateBooking(booking))
