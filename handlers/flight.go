d2bce8d30069d9b550acd24682ea7867c5ebdaf3
oktay <oktay@alizada.net>
2024-02-14 20:37:29 +0100

    fix: separate booking and flight handlers

--- handlers/flight.go
+++ handlers/flight.go
@@ -25,20 +25,3 @@ func CreateFlight(c echo.Context) (err error) {
 	}
 	return c.JSON(http.StatusOK, da.CreateNewFlight(flight))
 }
-
-func GetBookings(c echo.Context) (err error) {
-	return c.JSON(http.StatusOK, da.GetBookings())
-}
-
-func CreateBooking(c echo.Context) (err error) {
-	booking := new(models.Booking)
-	if err = c.Bind(booking); err != nil {
-		return echo.NewHTTPError(http.StatusOK, "can't bind")
-	}
-	return c.JSON(http.StatusOK, da.CreateBooking(booking))
-}
-
-func GetBooking(c echo.Context) (err error) {
-	bookId := c.Param("id")
-	return c.JSON(http.StatusOK, da.GetBooking(bookId))
-}
